1794b4a803b500d2d3585fadc7063b13f573431f
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2021-04-30 00:59:24 +0900

    Fix Databse

--- database/config.py
+++ database/config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///test.db"
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or "sqlite:///test.db"
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///test.db"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or "sqlite:///test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY="secret key"
